7ea4feebf8a6b025788016283972c3e1a10351e2
Dave Hill <dave@davebuilt.co.uk>
2015-08-04 21:21:23 +0100

    Modified the user_research table and added a user_ships table

--- database/migrations/2015_08_04_201647_create_user_ships_table.php
+++ database/migrations/2015_08_04_201647_create_user_ships_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserResearchTable extends Migration
+class CreateUserShipsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,12 @@ class CreateUserResearchTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_research', function (Blueprint $table) {
+        Schema::create('user_ships', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('ticks_remaining');
             $table->integer('user_id')->references('id')->on('users');
-            $table->integer('research_id')->references('id')->on('research');
+            $table->integer('ship_id')->references('id')->on('ships');
+            $table->integer('quantity');
+            $table->integer('quantity_pending');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateUserResearchTable extends Migration
      */
     public function down()
     {
-        Schema::drop('user_research');
+        Schema::drop('user_ships');
     }
 }
